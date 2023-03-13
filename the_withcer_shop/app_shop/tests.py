154fe5c6df75fc66602e1a8ec70501d8978c93cc
DemonMaike <dmitry.malyshevv@ya.ru>
2023-03-13 15:27:26 +0400

    Creted test on status pages 200, changed gitignore with gitignore.io, changed readme.md

--- the_withcer_shop/app_shop/tests.py
+++ the_withcer_shop/app_shop/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class Code200TestCase(TestCase):
+    """Class for testing of pages on status code 200"""
+
+    def test_home_200(self):
+        """Testing the homepage"""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+            
+    def test_about_200(self):
+        """Testing the about page"""
+        response = self.client.get('/about')
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_200(self):
+        """Testing the category page"""
+        response = self.client.get('/category')
+        self.assertEqual(response.status_code, 200)
+
+    def test_allproducts_200(self):
+        """Testing the allproducts page"""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
--- the_withcer_shop/app_shop/urls.py
+++ the_withcer_shop/app_shop/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
     path('category', views.category, name='category'),
-    path('allproducts', views.allproducts, 
-    name='allproducts'),
+    path('allproducts', views.allproducts, name='allproducts'),
 ]
