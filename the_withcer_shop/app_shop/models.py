23c1ec3d94f9a8e2ccb4b0c720193f2d05a83b7f
DemonMaike <dmitry.malyshevv@ya.ru>
2023-03-29 01:28:11 +0400

    Created models for db

--- the_withcer_shop/app_shop/models.py
+++ the_withcer_shop/app_shop/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    """Model for products"""
+    name = models.CharField(
+        max_length=255, 
+        verbose_name='Name of porduct'
+        )
+    description = models.TextField(
+        verbose_name='Descripton'
+        )
+    price = models.IntegerField(
+        verbose_name='Price'
+        )
+    photo = models.ImageField(
+        upload_to='Product/%Y/%m/%d',
+        verbose_name='Photo of products'
+        )
+    category = models.ForeignKey(
+        'Category',
+         on_delete=models.CASCADE
+        )
+    
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Category'
+    )
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+class Card(models.Model):
+    id_user = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE
+     )
+
+    class Meta:
+        verbose_name = 'Card'
+        verbose_name_plural = 'Cards'
+
+class Card_Product(models.Model):
+    id_card = models.ForeignKey(
+        'Card',
+         on_delete=models.CASCADE
+        )
+    id_product = models.ForeignKey(
+        'Product',
+        on_delete=models.CASCADE
+    )
+
